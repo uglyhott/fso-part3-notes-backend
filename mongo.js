51d0259905cdc9a12ac1546343c5cb480710a858
Liam Blumenstein-Tait <liam.bt@gmail.com>
2024-09-26 21:44:26 +0200

    add ESLint and fix problems

--- mongo.js
+++ mongo.js
@@ -14,8 +14,8 @@ mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
-    content: String,
-    important: Boolean,
+  content: String,
+  important: Boolean,
 })
 
 const Note = mongoose.model('Note', noteSchema)
@@ -31,8 +31,8 @@ const Note = mongoose.model('Note', noteSchema)
 // })
 
 Note.find({}).then(result => {
-    result.forEach(note => {
-        console.log(note)
-    })
-    mongoose.connection.close()
+  result.forEach(note => {
+    console.log(note)
+  })
+  mongoose.connection.close()
 })

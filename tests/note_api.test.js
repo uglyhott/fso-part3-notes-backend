c239b6df84d7b8a76634e9d3ac24f5d2b9885621
Liam Blumenstein-Tait <liam.bt@gmail.com>
2024-10-09 21:09:07 +0200

    init DB before tests

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -1,10 +1,36 @@
 const mongoose = require('mongoose')
-const { test, after } = require('node:test')
+const { test, after, beforeEach } = require('node:test')
 const assert = require('node:assert')
 const supertest = require('supertest')
 const app = require('../app')
+const Note = require('../models/note')
+const { MONGODB_URI } = require('../utils/config')
 
 const api = supertest(app)
+
+const initialNotes = [
+  {
+    content: 'HTML is easy',
+    important: false,
+  },
+  {
+    content: 'Browser can execute only JavaScript',
+    important: true,
+  },
+]
+
+mongoose.set('strictQuery', false)
+mongoose.connect(MONGODB_URI)
+
+beforeEach(async () => {
+  await Note.deleteMany({})
+  let noteObject = new Note(initialNotes[0])
+  await noteObject.save()
+  noteObject = new Note(initialNotes[1])
+  await noteObject.save()
+})
+
+console.log('In note_api.test.js, NODE_ENV is set to', process.env.NODE_ENV)
 
 test('notes are returned as json', async () => {
   await api
@@ -20,7 +46,7 @@ after(async () => {
 test('there are 2 notes', async () => {
   const response = await api.get('/api/notes')
 
-  assert.strictEqual(response.body.length, 2)
+  assert.strictEqual(response.body.length, initialNotes.length)
 })
 
 test('there is a note on HTML', async () => {
--- utils/logger.js
+++ utils/logger.js
@@ -11,5 +11,5 @@ const error = (...params) => {
 }
 
 module.exports = {
-  info, error
+  info, error,
 }
